4257edf5779d742a90e5da06f116c1fb7e01db04
wlsgurrla10 <wlsgurrla10@gmail.com>
2022-03-17 11:15:02 +0900

    browserrouter를 hashrouter로 바꿔 github page에서 동작하지 않는 현상 수정

--- src/router.tsx
+++ src/router.tsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import Home from "./routes/Home";
 import MbitTest from "./routes/MbtiTest";
 import TestResult from "./routes/TestResult";
 
 function MbtiRouter(){
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path="/mbtitest/result/:result" element={<TestResult />} />
                 <Route path="/mbtitest" element={<MbitTest />} />
                 <Route path="/" element={<Home />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
 
